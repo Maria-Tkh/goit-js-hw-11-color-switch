8bcd770b14187619c438262c0f09353aac50be5d
Maria-Tkh <mcweaver117@gmail.com>
2021-07-05 23:33:55 +0300

    hw-11_color-switcher

---
+++ colors.js
@@ -0,0 +1,46 @@
+const colors = [
+    '#FFFFFF',
+    '#2196F3',
+    '#4CAF50',
+    '#FF9800',
+    '#009688',
+    '#795548',
+];
+
+const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const body = document.querySelector('body');
+
+const refs = {
+    startBtn: document.querySelector('[data-action="start"]'),
+    stopBtn: document.querySelector('[data-action="stop"]'),
+}
+createColors = () => {
+    refs.startBtn.setAttribute('disabled')
+    const color = randomIntegerFromInterval();
+    body.style.backgroundColor = color;
+    refs.startBtn.removeAttribute('disabled');
+}
+
+
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+let intervalId = null;
+
+function onStartBtnClick() {
+    // console.log('click');
+    intervalId = setInterval(() => {
+        createColors();
+    }, 1000);
+}
+
+
+function onStopBtnClick() {
+    // console.log('click2');
+    clearInterval(intervalId);
+}
+
